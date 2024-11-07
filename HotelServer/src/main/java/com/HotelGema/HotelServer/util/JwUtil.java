558a6d384b05f1241c122e5b2a3da2903f3a338f
ManriqueR <manrique.recinos23@gmail.com>
2024-11-07 15:32:07 -0600

    Analisis y Validacion de JWT

--- HotelServer/src/main/java/com/HotelGema/HotelServer/util/JwUtil.java
+++ HotelServer/src/main/java/com/HotelGema/HotelServer/util/JwUtil.java
@@ -1,5 +1,6 @@
 package com.HotelGema.HotelServer.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwUtil {
@@ -22,8 +25,37 @@ public class JwUtil {
                 .signWith(SignatureAlgorithm.HS256,getSigningKey()).compact();
     }
 
-    private Key getSigningKey() {
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode("413F4428472B4B625065536858566D59703373367639792442264529484D6351");
         return Keys.hmacShaKeyFor(keyBytes);
     }
